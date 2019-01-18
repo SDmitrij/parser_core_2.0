a61567d22ace23196693b7f969d95aa082921a6d
Dmitry <sidelevdima@yandex.ua>
2019-01-18 19:45:15 +0300

    remove some files

--- js/main.js
+++ js/main.js
@@ -2,23 +2,24 @@ $(function () {
     $('.parser-core_input_send_data').click(function () {
         let sendData = $('.parser-core_search_by_word_input').val();
         if (sendData != null && sendData !== '') {
-            console.log(sendData);
             $.ajax({
                 url: 'helper.php',
                 type: 'post',
                 dataType: 'json',
                 data: {wordToSearch: sendData},
                 success: function (dataToRender) {
-
                     let htmlToRender = "<div class='parser-core_search_results'>";
                     htmlToRender += dataToRender['file_info'];
+
                     for (let i = 0; i < dataToRender['file_strings'].length; i++)
                     {
                        htmlToRender += dataToRender['file_strings'][i];
                     }
+
                     htmlToRender += "</div>";
 
                     $('.parser-core_search_results').remove();
+
                     $('.parser-core_search_block').append(htmlToRender);
                 }
             })
